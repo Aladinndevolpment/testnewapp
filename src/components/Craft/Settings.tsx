ddd76fc13cc4837ddf7521536ecc7b3d3d4cfc6b
chahat <chahat@aladinntech.in>
2023-06-05 17:53:43 +0530

    master

--- src/components/Craft/Settings.tsx
+++ src/components/Craft/Settings.tsx
@@ -13,7 +13,7 @@ export const SettingsPanel = () => {
     if (currentNodeId) {
       selected = {
         id: currentNodeId,
-        name: state.nodes[currentNodeId].data.displayName,
+        name: state.nodes[currentNodeId].data.name,
         settings:
           state.nodes[currentNodeId].related &&
           state.nodes[currentNodeId].related.settings,
